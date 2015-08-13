be8af87fba60ec2fc587da5320721d7aae3fd39a
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-13 15:12:32 +0800

    use fixtures to classfiy the data send to the controller

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,22 +1,24 @@
-var _ = require('lodash');
-var initialData = require('../seeds/initial-data');
 var loadAllQuestion = require('../config/fixtures');
+var initialData = require('../seeds/initial-data');
 
 function rederIndexPage(req, res, data) {
     res.render('index', data);
 }
 
-function getIndexInfo(req,res) {
-    loadAllQuestion();
-
-    // rederIndexPage(req,res,{
-    //     data : initialData
-    // });
+function getIndexInfo(req, res) {
+    var data = loadAllQuestion();
+    console.log(data);
+    rederIndexPage(req, res, {
+        userData: initialData,
+        data: data
+    });
 }
 
-function getScore(req,res) {
-    console.log(req.body);
-    rederIndexPage(req,res);
+function getScore(req, res) {
+    var userData = req.body;
+    rederIndexPage(req, res, {
+        userData: userData
+    });
 }
 
 module.exports = {
