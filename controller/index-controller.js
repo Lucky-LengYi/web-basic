ea49da2fb5662394aa782cdac0e5556d5777ac50
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-13 13:23:01 +0800

    add the initialData in seeds

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,14 +1,18 @@
 var _ = require('lodash');
+var initialData = require('../seeds/initial-data');
 
 function rederIndexPage(req, res, data) {
     res.render('index', data);
 }
 
 function getIndexInfo(req,res) {
-    rederIndexPage(req,res);
+    rederIndexPage(req,res,{
+        data : initialData
+    });
 }
 
 function getScore(req,res) {
+    console.log(req.body);
     rederIndexPage(req,res);
 }
 
