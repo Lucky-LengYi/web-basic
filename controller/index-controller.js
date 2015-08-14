5f1c7970072c9cd1ae1ce856ef915fceedbae13c
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-15 01:12:19 +0800

    change the controller to the class

--- controller/index-controller.js
+++ controller/index-controller.js
@@ -1,38 +1,44 @@
+'use strict';
+
 var loadAllQuestion = require('../config/fixtures');
 var initialData = require('../seeds/initial-data');
-var HelperTopic = require('../helper/helper-topic');
+var TopicHelper = require('../helper/topic-helper');
 
-function rederIndexPage(req, res, data) {
+function IndexController(argument) {
+
+}
+
+function renderIndexPage(req, res, data) {
     res.render('index', data);
 }
 
-function getIndexInfo(req, res) {
-    var data = loadAllQuestion();
-    var helperTopic = new HelperTopic();
+IndexController.index = function(req, res) {
+
+    var topicHelper = new TopicHelper();
+    var topic = topicHelper.getTopic();
+    
     data = helperTopic.classify(data);
 
-    rederIndexPage(req, res, {
+    renderIndexPage(req, res, {
         userData: initialData,
         data: data
     });
 }
 
-function getScore(req, res) {
-    var userData = req.body;
+IndexController.submit = function(req, res) {
     var data = loadAllQuestion();
+    var userData = req.body;
     var helperTopic = new HelperTopic();
 
     helperTopic.fractionalStatistics(userData,data);
     helperTopic.gatValue(userData,data);
+
     data = helperTopic.classify(data);
 
-    rederIndexPage(req, res, {
+    renderIndexPage(req, res, {
         userData: userData,
         data:data
     });
 }
 
-module.exports = {
-    getIndexInfo: getIndexInfo,
-    getScore: getScore
-};
+module.exports = IndexController;
