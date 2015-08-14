c019744b111da815f4bfb710119593118dcf37d3
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-15 03:06:58 +0800

    finish the view model

---
+++ view-models/index.js
@@ -0,0 +1,44 @@
+'use strict';
+
+var FillTopic = require('../model/fill-topic');
+var SingleChoiceTopic = require('../model/single-choice-topic');
+var MultipleChoiceTopic = require('../model/multiple-choice-topic');
+var TrueOrFalseTopic = require('../model/true-or-false-topic');
+var ShortAnswerTopic = require('../model/short-answer-topic');
+
+function IndexViewModel(topics,userData) {
+    this.topics = topics;
+    this.userData = userData;
+}
+
+IndexViewModel.prototype.getFillTopics = function() {
+    return this.topics.filter(function(topic) {
+        return topic instanceof FillTopic;
+    });
+};
+
+IndexViewModel.prototype.getSingleChoiceTopics = function() {
+    return this.topics.filter(function(topic) {
+        return topic instanceof SingleChoiceTopic;
+    });
+};
+
+IndexViewModel.prototype.getMultipleChoiceTopics = function() {
+    return this.topics.filter(function(topic) {
+        return topic instanceof MultipleChoiceTopic;
+    });
+};
+
+IndexViewModel.prototype.getTrueOrFalseTopics = function() {
+    return this.topics.filter(function(topic) {
+        return topic instanceof TrueOrFalseTopic;
+    });
+};
+
+IndexViewModel.prototype.getShortAnswerTopics = function() {
+    return this.topics.filter(function(topic) {
+        return topic instanceof ShortAnswerTopic;
+    });
+};
+
+module.exports = IndexViewModel;
