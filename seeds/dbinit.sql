f4cb6759318afc84777b395bfc89509f70cffd65
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-18 23:49:29 +0800

    change the table options cloumn name opt to title

--- seeds/dbinit.sql
+++ seeds/dbinit.sql
@@ -17,8 +17,8 @@ CREATE TABLE question
 
 CREATE TABLE options
 (
-        id int not null auto_increment,
-    opt varchar(20),
+    id int not null auto_increment,
+    title varchar(20),
     val varchar(50),
     question_id int not null,
     PRIMARY KEY (id),
@@ -35,36 +35,36 @@ insert into question(id,name,question,answer,score,classes) values(7,'questionSe
 insert into question(id,name,question,answer,score,classes) values(8,'questionEight','在状态图中,终止状态在一个状态图中允许有任意多个。','true',2,'TrueOrFalse');
 insert into question(id,name,question,answer,score,classes) values(9,'questionNine','简述什么是模型以及模型的表现形式?','',5,'ShortAnswer');
 
-insert into options(opt,val,question_id) values(null,null,1);
+insert into options(title,val,question_id) values(null,null,1);
 
-insert into options(opt,val,question_id) values(null,null,2);
-insert into options(opt,val,question_id) values(null,null,2);
-insert into options(opt,val,question_id) values(null,null,2);
+insert into options(title,val,question_id) values(null,null,2);
+insert into options(title,val,question_id) values(null,null,2);
+insert into options(title,val,question_id) values(null,null,2);
 
-insert into options(opt,val,question_id) values('A','UML 就是软件工程',3);
-insert into options(opt,val,question_id) values('B','UML 参与到软件工程中软件开发过程的几个阶段',3);
-insert into options(opt,val,question_id) values('C','UML 与软件工程无关',3);
-insert into options(opt,val,question_id) values('D','UML 是软件工程的一部分',3);
+insert into options(title,val,question_id) values('A','UML 就是软件工程',3);
+insert into options(title,val,question_id) values('B','UML 参与到软件工程中软件开发过程的几个阶段',3);
+insert into options(title,val,question_id) values('C','UML 与软件工程无关',3);
+insert into options(title,val,question_id) values('D','UML 是软件工程的一部分',3);
 
-insert into options(opt,val,question_id) values('A','单继承',4);
-insert into options(opt,val,question_id) values('B','多继承',4);
-insert into options(opt,val,question_id) values('C','单继承和多继承都支持',4);
-insert into options(opt,val,question_id) values('D','单继承和多继承都不支持',4);
+insert into options(title,val,question_id) values('A','单继承',4);
+insert into options(title,val,question_id) values('B','多继承',4);
+insert into options(title,val,question_id) values('C','单继承和多继承都支持',4);
+insert into options(title,val,question_id) values('D','单继承和多继承都不支持',4);
 
-insert into options(opt,val,question_id) values('A','概述级',5);
-insert into options(opt,val,question_id) values('B','需求级',5);
-insert into options(opt,val,question_id) values('C','用户目标级',5);
-insert into options(opt,val,question_id) values('D','子功能级',5);
+insert into options(title,val,question_id) values('A','概述级',5);
+insert into options(title,val,question_id) values('B','需求级',5);
+insert into options(title,val,question_id) values('C','用户目标级',5);
+insert into options(title,val,question_id) values('D','子功能级',5);
 
-insert into options(opt,val,question_id) values('A','名称(Name)',6);
-insert into options(opt,val,question_id) values('B','属性(Attribute)',6);
-insert into options(opt,val,question_id) values('C','操作(Operation)',6);
-insert into options(opt,val,question_id) values('D','方法(Function)',6);
+insert into options(title,val,question_id) values('A','名称(Name)',6);
+insert into options(title,val,question_id) values('B','属性(Attribute)',6);
+insert into options(title,val,question_id) values('C','操作(Operation)',6);
+insert into options(title,val,question_id) values('D','方法(Function)',6);
 
-insert into options(opt,val,question_id) values('true','正确',7);
-insert into options(opt,val,question_id) values('false','错误',7);
+insert into options(title,val,question_id) values('true','正确',7);
+insert into options(title,val,question_id) values('false','错误',7);
 
-insert into options(opt,val,question_id) values('true','正确',8);
-insert into options(opt,val,question_id) values('false','错误',8);
+insert into options(title,val,question_id) values('true','正确',8);
+insert into options(title,val,question_id) values('false','错误',8);
 
-insert into options(opt,val,question_id) values(null,null,9);
+insert into options(title,val,question_id) values(null,null,9);
