69b6e07f495db9891517795c3fa9a83baa3023ba
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-19 10:24:05 +0800

    add the gulp start

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp');
 var config = require('./config/database');
 var execsql = require('execsql');
+var nodemon = require('nodemon');
+var exec = require('child_process').exec;
+
+var path = ['./controller/*', './config/*', './helper/*', './model/*', './router/*','./view-models/*','./lib/*','./*.js'];
 
 gulp.task('dbinit', function () {
     dbConfig = {
@@ -9,11 +13,18 @@ gulp.task('dbinit', function () {
         password: 'root'
     },
     sqlFile = __dirname + '/seeds/dbinit.sql';
-
     var conn = execsql.config(dbConfig);
-
     conn.execFile(sqlFile, function(err, results){
-        console.log(results);
+        console.log('error :' + err);
         conn.end();
     });
+});
+
+gulp.task('start', function() {
+    exec('node app.js');
+});
+
+gulp.task('default', function() {
+    gulp.start('start');
+    gulp.watch(path, ['start']);
 });
